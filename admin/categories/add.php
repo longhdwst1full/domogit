7adcbefefe90ba04a12e6560ab8ad8b5fb0648ed
long1703@outlook.com <long1703@outlook.com>
2022-09-19 23:57:03 +0700

    form add categories

---
+++ admin/categories/add.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="row frmtitle">
+        <h1>Thêm mới loại hàng hóa</h1>
+    </div>
+    <div class="row frmcontent">
+        <form action="index.php?act=add" method="post" enctype="multipart/form-data">
+            <div class="row mb10">
+                Mã loại <br>
+                <input type="text" name="id" disabled>
+
+            </div>
+            <div class="row mb10">
+                Tên loại <br>
+                <input type="text" name="id" >
+                <div class="row mb10">
+                    <input type="submit" name="themmoi" value="Thêm mới">
+                    <input type="reset" value="Nhập lại">
+                    <a href="danhsach_loaihang.html">
+                        <input type="button" value="Danh sách0">
+                    </a>
+                </div>
+            </div>
+        </form>
+    </div>
+
+</div>
